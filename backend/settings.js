b41ac2ca735e835d9d1ec177c37ac23c797f976b
Bo Henriksen <bomarconi@gmail.com>
2018-11-13 19:17:51 +0100

    Delete settings.js

--- backend/settings.js
+++
@@ -1,9 +0,0 @@
-const DEV_DB_URI = "mongodb://gregory:brandy2@ds261253.mlab.com:61253/server_attack";
-// const TEST_DB_URI = "mongodb://BenedikteEva:sg04pbem@ds247439.mlab.com:47439/miniprojecttest";
-// const MOCHA_TEST_TIMEOUT =5000;
-
-module.exports = {
-  DEV_DB_URI,
-//  TEST_DB_URI,
-//  MOCHA_TEST_TIMEOUT
-}
